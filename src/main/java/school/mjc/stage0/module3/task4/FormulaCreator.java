01a909894d8168414d5b5e7d3bcedaabcad3f550
Vlad Kupchik <vladkupchik15@gmail.com>
2023-07-03 00:43:01 +0300

    123

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 2, b = 4;
+        double c = (9 * Math.pow(a, 2) - 5 * b + a - 7)*((a + b - 4 * a * b) / 2);
+        System.out.println(c);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int lettersC = 'c', lettersH = 'h', lettersA = 'a', lettersR = 'r';
+        System.out.println(lettersC + " " + lettersH + " " + lettersA + " " + lettersR);
     }
 }
